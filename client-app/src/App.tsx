602aba0ae8c94ccf4d5920c014da65d4695e17ef
Sangseok Doe <sangseok.doe@rlcarriers.com>
2021-03-11 17:21:30 -0500

    Finished section 4

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 import axios from "axios";
-import { Header, List, ListItem } from 'semantic-ui-react';
+import { Header, List } from "semantic-ui-react";
 
 function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then(response => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
       setActivities(response.data);
-    })
+    });
   }, []);
 
   return (
@@ -18,11 +17,8 @@ function App() {
       <Header as="h2" icon="users" content="Reactivities" />
       <List>
         {activities.map((activity: any) => (
-          <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          )
-        )}
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
       </List>
     </div>
   );
